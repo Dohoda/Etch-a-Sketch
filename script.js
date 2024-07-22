505d3862c85ddaaa71ec6527d21a8580bd5de6cc
Dohoda <173878821+Dohoda@users.noreply.github.com>
2024-07-22 08:42:18 +0300

    added styles to boxes using js.

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container");
+
+const box = document.createElement("div");
+box.classList.add("box");
+
+box.style.backgroundColor = "cornflowerblue";
+box.style.height = "6.25%";
+box.style.width = "6.25%";
+
+container.appendChild(box);
