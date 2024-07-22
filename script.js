955018f1457e1a29d630a5d1a91131e300b1478d
Dohoda <173878821+Dohoda@users.noreply.github.com>
2024-07-22 08:45:01 +0300

    created getSize function for box width and height.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+function getSize(amount){
+    return 100 / amount;
+}
+
+let gridSize = getSize(16);
+
+
 const container = document.querySelector(".container");
 
 const box = document.createElement("div");
 box.classList.add("box");
 
 box.style.backgroundColor = "cornflowerblue";
-box.style.height = "6.25%";
-box.style.width = "6.25%";
+box.style.height = gridSize;
+box.style.width = gridSize;
 
 container.appendChild(box);
