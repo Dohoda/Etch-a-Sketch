ff82857bba569712cd793934770526352081518b
Dohoda <173878821+Dohoda@users.noreply.github.com>
2024-07-23 08:28:00 +0300

    added class name "box" to boxes created.

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ let column = 16;
 
 for(let i = 0; i < row; i++){
     const box = document.createElement("div");
+    box.classList.add("box");
     box.style.height = "5%";
     box.style.width = "5%";
     box.style.border = "1px solid black";
-    
+
     container.appendChild(box);
 }
 
 for(let j = 0; j < column; j++){
     const box = document.createElement("div");
+    box.classList.add("box");
     box.style.height = "5%";
     box.style.width = "5%";
     box.style.border = "1px solid black";
