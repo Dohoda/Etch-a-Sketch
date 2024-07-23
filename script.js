b40f621dac2cacbf41798dd6845da41055aa7b30
Dohoda <173878821+Dohoda@users.noreply.github.com>
2024-07-23 08:38:29 +0300

    moved everything under buildGrid function.

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const container = document.querySelector("div");
 let row = 16;
 let column = 16;
 
-for(let i = 0; i < row; i++){
-    const box = document.createElement("div");
-    box.classList.add("row");
-    box.style.border = "1px solid black";
+function buildGrid(){
+ for(let i = 0; i < row; i++){
+     const box = document.createElement("div");
+     box.classList.add("row");
+     box.style.border = "1px solid black";
 
-    container.appendChild(box);
-}
+     container.appendChild(box);
 
-for(let j = 0; j < column; j++){
-    const box = document.createElement("div");
-    box.classList.add("column");
-    box.style.border = "1px solid black";
-
-    container.appendChild(box);
+     for(let j = 0; j < column; j++){
+         const box = document.createElement("div");
+         box.classList.add("column");
+         box.style.border = "1px solid black";
+    
+         container.appendChild(box);
+     }
+ }
 }
