4850c0d455ec3d060376375dc43ef6b0ce9916de
Dohoda <173878821+Dohoda@users.noreply.github.com>
2024-07-23 08:47:50 +0300

    fixed row and column generation.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const container = document.querySelector("div");
+const container = document.querySelector(".container");
 
 let row = 16;
 let column = 16;
 
 function buildGrid(){
  for(let i = 0; i < row; i++){
-     const box = document.createElement("div");
-     box.classList.add("row");
-     box.style.border = "1px solid black";
+     const row = document.createElement("div");
+     row.classList.add("row");
+     
 
-     container.appendChild(box);
+     container.appendChild(row);
 
      for(let j = 0; j < column; j++){
-         const box = document.createElement("div");
-         box.classList.add("column");
-         box.style.border = "1px solid black";
+         const column = document.createElement("div");
+         column.classList.add("column");
+         
     
-         container.appendChild(box);
-     }
- }
+         row.appendChild(column);
+        }
+    }
 }
 
 buildGrid();
